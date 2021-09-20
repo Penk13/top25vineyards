4fb9cf77b65eccec1c72fa1c75f5d83bb003e8fb
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-09-20 14:30:15 +0700

    heroku - postgres (database)

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,4 +1,8 @@
+import dj_database_url
 from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['topvineyards.herokuapp.com']
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
