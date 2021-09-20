9b3f9e49e9e99905f9b78846fce2ab49761dd84a
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-09-20 15:12:31 +0700

    security

--- base/settings/base.py
+++ base/settings/base.py
@@ -137,3 +137,14 @@ MEDIA_ROOT = BASE_DIR / "static-live" / "media-root"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_REPLACE_HTTPS_REFERER = False
+HOST_SCHEME = "http://"
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_SECONDS = None
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_FRAME_DENY = False
--- base/settings/production.py
+++ base/settings/production.py
@@ -1,5 +1,8 @@
+import os
 import dj_database_url
 from .base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 DEBUG = False
 ALLOWED_HOSTS = ['topvineyards.herokuapp.com']
@@ -7,6 +10,14 @@ ALLOWED_HOSTS = ['topvineyards.herokuapp.com']
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-MIDDLEWARE += [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 1000000
+SECURE_FRAME_DENY = True
