2dfb495b6b7ead7b55cf4ea9f93030d28f28015e
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-09-22 18:49:46 +0700

    add link to logo and add top by regions to navbar

--- pages_app/views.py
+++ pages_app/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.core.paginator import Paginator
 from .models import ContentPage
-from vineyards.models import Vineyard
+from vineyards.models import Vineyard, Region
 
 
 def mainpage(request):
     homepage = get_object_or_404(ContentPage, types="HOME_PAGE")
+    navbar_region = get_list_or_404(Region)
     p = Paginator(Vineyard.objects.all(), 3)
     page = request.GET.get('page')
     vineyards = p.get_page(page)
-    context = {"homepage": homepage, "vineyards": vineyards}
+    context = {"homepage": homepage,
+               "navbar_region": navbar_region, "vineyards": vineyards}
     return render(request, "pages_app/main_page.html", context)
 
 
