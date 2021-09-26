47d7172f4bef25c3fd560ac43924bd602e7ee5ad
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-09-26 21:37:10 +0700

    add filter by region

--- vineyards/urls.py
+++ vineyards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import (vineyard_detail)
+from .views import (vineyard_detail, vineyard_region)
 
 app_name = 'vineyards'
 urlpatterns = [
-    path('detail/<int:pk>/', vineyard_detail, name="detail")
+    path('detail/<int:pk>/', vineyard_detail, name="detail"),
+    path('<slug:slug>/', vineyard_region, name="region"),
 ]
--- vineyards/views.py
+++ vineyards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Vineyard
+from django.core.paginator import Paginator
+from .models import Vineyard, Region
 
 
 def vineyard_detail(request, pk):
@@ -8,5 +9,11 @@ def vineyard_detail(request, pk):
     return render(request, "vineyards/vineyard.html", context)
 
 
-def vineyard_region(request):
-    return render(request)
+def vineyard_region(request, slug):
+    region = get_object_or_404(Region, slug=slug)
+    p = Paginator(Vineyard.objects.filter(
+        region=region).order_by("-rating"), 10)
+    page = request.GET.get('page')
+    vineyards = p.get_page(page)
+    context = {"vineyards": vineyards, "region": region}
+    return render(request, "vineyards/vineyard_region.html", context)
