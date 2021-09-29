85392881cada8d666d9f022cf6507fd9409a3535
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-09-29 13:07:15 +0700

    add image carousel on region page

--- vineyards/views.py
+++ vineyards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from .models import Vineyard, Region
+from .models import Vineyard, Region, RegionImage
 
 
 def vineyard_detail(request, pk):
@@ -11,9 +11,11 @@ def vineyard_detail(request, pk):
 
 def vineyard_region(request, slug):
     region = get_object_or_404(Region, slug=slug)
+    region_images = RegionImage.objects.filter(region=region)
     p = Paginator(Vineyard.objects.filter(
         region=region).order_by("-rating"), 10)
     page = request.GET.get('page')
     vineyards = p.get_page(page)
-    context = {"vineyards": vineyards, "region": region}
+    context = {"vineyards": vineyards, "region": region,
+               "region_images": region_images}
     return render(request, "vineyards/vineyard_region.html", context)
