596951e77906cbd0e4f9ff4da09fad96407be082
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-10-03 11:44:06 +0700

    pages app type footer

--- pages_app/context_processors.py
+++ pages_app/context_processors.py
@@ -11,10 +11,10 @@ def base_variable(request):
     navbar_region_child = Region.objects.filter(
         display_on_navbar=True, region_parent=True)
     travel_news = Post.objects.all()
-    footer_count = ContentPage.objects.filter(types="PAGE").count()
+    footer_count = ContentPage.objects.filter(types="FOOTER").count()
     footer_index = int(footer_count/2)
-    footers1 = get_list_or_404(ContentPage, types="PAGE")[:footer_index]
-    footers2 = get_list_or_404(ContentPage, types="PAGE")[footer_index:]
+    footers1 = ContentPage.objects.filter(types="FOOTER")[:footer_index]
+    footers2 = ContentPage.objects.filter(types="FOOTER")[footer_index:]
     return {"navbars": navbars,
             "navbar_region": navbar_region,
             "navbar_region_child": navbar_region_child,
