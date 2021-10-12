b554eab3493014397665308f43ac5b49cfd9c2fd
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-10-12 21:23:30 +0700

    autoblogging access only for superuser and travel news slider ordered from newest

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Autoblogging, Category
 
@@ -11,32 +12,38 @@ def news_detail(request, category, news):
 
 
 def autoblogging(request):
-    sources = Autoblogging.objects.all()
-    context = {"sources": sources}
-    return render(request, "news/autoblogging.html", context)
+    if request.user.is_superuser:
+        sources = Autoblogging.objects.all()
+        context = {"sources": sources}
+        return render(request, "news/autoblogging.html", context)
+    else:
+        return HttpResponse("Sorry you are not allowed to access this page")
 
 
 def pull_feeds(request, pk):
-    source = Autoblogging.objects.get(pk=pk)
+    if request.user.is_superuser:
+        source = Autoblogging.objects.get(pk=pk)
 
-    url = requests.get(source.url)
-    soup = BeautifulSoup(url.content, "html.parser")
-    length = source.items
-    items = soup.find_all('item')[:length]
-    contents = soup.find_all('content:encoded')[:length]
-    covers = soup.find_all('media:content')[:length]
+        url = requests.get(source.url)
+        soup = BeautifulSoup(url.content, "html.parser")
+        length = source.items
+        items = soup.find_all('item')[:length]
+        contents = soup.find_all('content:encoded')[:length]
+        covers = soup.find_all('media:content')[:length]
 
-    for i in range(length-1, -1, -1):
-        title = items[i].title.text
-        body = contents[i].text
-        try:
-            cover = covers[i]['url']
-        except:
-            cover = ""
-        category = Category.objects.get(pk=source.category.id)
-        if not Post.objects.filter(title=title).exists():
-            Post.objects.create(title=title,
-                                body=str(body),
-                                cover=str(cover),
-                                category=category)
-    return redirect("news:autoblogging")
+        for i in range(length-1, -1, -1):
+            title = items[i].title.text
+            body = contents[i].text
+            try:
+                cover = covers[i]['url']
+            except:
+                cover = ""
+            category = Category.objects.get(pk=source.category.id)
+            if not Post.objects.filter(title=title).exists():
+                Post.objects.create(title=title,
+                                    body=str(body),
+                                    cover=str(cover),
+                                    category=category)
+        return redirect("news:autoblogging")
+    else:
+        return HttpResponse("Sorry you are not allowed to access this page")
--- pages_app/context_processors.py
+++ pages_app/context_processors.py
@@ -5,7 +5,7 @@ from news.models import Post, Category
 def base_variable(request):
     navbars = Navbar.objects.all().order_by("order")
     category = Category.objects.get(slug="global-travel-news")
-    travel_news = Post.objects.filter(category=category)
+    travel_news = Post.objects.filter(category=category).order_by("-id")
     footer_count = ContentPage.objects.filter(types="FOOTER").count()
     footer_index = int(footer_count/2)
     footers1 = ContentPage.objects.filter(types="FOOTER")[:footer_index]
