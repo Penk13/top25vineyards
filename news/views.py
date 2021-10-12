f55cb2334a27c05f3943017d054873b49f077cc7
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-10-12 11:11:21 +0700

    create pull feed function using bs4

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post, Autoblogging
+from .models import Post, Autoblogging, Category
 
 
 def news_detail(request, category, news):
@@ -14,3 +14,33 @@ def autoblogging(request):
     sources = Autoblogging.objects.all()
     context = {"sources": sources}
     return render(request, "news/autoblogging.html", context)
+
+
+def pull_feeds(request, pk):
+    url = requests.get("https://travelcommunication.net/feed/")
+
+    soup = BeautifulSoup(url.content, "html.parser")
+    length = 15
+    items = soup.find_all('item')[:length]
+    contents = soup.find_all('content:encoded')[:length]
+    covers = soup.find_all('media:content')[:length]
+    for i in range(length):
+        title = items[i].title.text
+        body = contents[i].text
+        # body_on_list = items[i].description.text
+        cover = covers[i]['url']
+        category = Category.objects.get(pk=pk)
+        # print("====================================================================")
+        # print(title)
+        # print(body)
+        # print(body_on_list)
+        # print(cover)
+        # print(category)
+        # print("====================================================================")
+        Post.objects.create(title=title,
+                            body=str(body),
+                            # body_on_list=str(body_on_list),
+                            cover=str(cover),
+                            category=category)
+    # Autoblogging.objects.get()
+    return redirect("news:autoblogging")
