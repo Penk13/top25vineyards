c8ad21d184045c554b25c9db6fc356c70f8152b4
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-10-16 14:42:15 +0700

    make slug on news category read only on admin

--- news/admin.py
+++ news/admin.py
@@ -6,7 +6,11 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('slug',)
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug',)
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag)
 admin.site.register(Autoblogging)
 admin.site.register(Post, PostAdmin)
