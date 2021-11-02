49d1fd9c1df5cacd0300b01eb3f656645bea4728
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-11-02 14:17:40 +0700

    move rating and review form to new page

--- vineyards/urls.py
+++ vineyards/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import (vineyard_detail, vineyard_region)
+from .views import (vineyard_detail, vineyard_region, rr_form)
 
 app_name = 'vineyards'
 urlpatterns = [
@@ -8,5 +8,9 @@ urlpatterns = [
          vineyard_detail, name="detail"),
     path('<str:region>/<slug:slug>/',
          vineyard_detail, name="detail-without-parent"),
+    path('<str:parent>/<str:region>/<slug:slug>/form/',
+         rr_form, name="detail-form"),
+    path('<str:region>/<slug:slug>/form/',
+         rr_form, name="detail-without-parent-form"),
     path('<slug:slug>/', vineyard_region, name="region"),
 ]
--- vineyards/views.py
+++ vineyards/views.py
@@ -11,27 +11,11 @@ def vineyard_detail(request, region, slug, parent=None):
     review_and_rating = ReviewAndRating.objects.filter(vineyard=vineyard)
     recent_reviews = ReviewAndRating.objects.filter(
         vineyard=vineyard).order_by('-id')[:3]
-    try:
-        obj = ReviewAndRating.objects.get(user=request.user, vineyard=vineyard)
-        form = ReviewRatingForm(request.POST or None, instance=obj)
-    except:
-        obj = None
-        form = ReviewRatingForm(request.POST or None)
-    if form.is_valid():
-        if obj:
-            form.save()
-        else:
-            instance = form.save(commit=False)
-            instance.user = request.user
-            instance.vineyard = vineyard
-            instance.save()
-        # form = ReviewRatingForm()
     context = {"vineyard": vineyard,
                "yard_images": yard_images,
                "yard_cover_images": yard_cover_images,
                "review_and_rating": review_and_rating,
                "recent_reviews": recent_reviews,
-               "form": form,
                }
     return render(request, "vineyards/vineyard.html", context)
 
@@ -46,3 +30,33 @@ def vineyard_region(request, slug):
     context = {"vineyards": vineyards, "region": region,
                "region_images": region_images}
     return render(request, "vineyards/vineyard_region.html", context)
+
+
+def rr_form(request, region, slug, parent=None):
+    vineyard = get_object_or_404(Vineyard, slug=slug)
+    yard_images = TopSliderImage.objects.filter(vineyard=vineyard)
+    yard_cover_images = CoverSliderImage.objects.filter(vineyard=vineyard)
+    recent_reviews = ReviewAndRating.objects.filter(
+        vineyard=vineyard).order_by('-id')[:3]
+    try:
+        obj = ReviewAndRating.objects.get(user=request.user, vineyard=vineyard)
+        form = ReviewRatingForm(request.POST or None, instance=obj)
+    except:
+        obj = None
+        form = ReviewRatingForm(request.POST or None)
+    if form.is_valid():
+        if obj:
+            form.save()
+        else:
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.vineyard = vineyard
+            instance.save()
+        return redirect(vineyard.get_absolute_url())
+    context = {"vineyard": vineyard,
+               "yard_images": yard_images,
+               "yard_cover_images": yard_cover_images,
+               "recent_reviews": recent_reviews,
+               "form": form,
+               }
+    return render(request, "vineyards/rr_form.html", context)
