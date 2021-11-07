b176097fb3fbfeaef106cfca1da5a70fd2b58fa5
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-11-07 19:55:13 +0700

    create profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Profile
+from .forms import ProfileForm
+from django.contrib.auth.decorators import login_required
+from pages_app.models import ContentPage
 
-# Create your views here.
+@login_required
+def profile(request):
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        print("POST")
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            print("VALID")
+            form.save()
+            return redirect("accounts:login")
+    content_page = get_object_or_404(ContentPage, types="SEARCH_PAGE")
+    context = {
+        'user': user,
+        'profile': profile,
+        'content_page': content_page,
+        'form': form,
+    }
+    return render(request, "account/profile.html", context)
--- base/urls.py
+++ base/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include("pages_app.urls")),
     path('vineyards/', include("vineyards.urls")),
     path('news/', include("news.urls")),
