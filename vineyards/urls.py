081d26df9757286875c79defeacbc059c47c45e8
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-11-07 22:48:12 +0700

    little fix on vineyard urls

--- vineyards/urls.py
+++ vineyards/urls.py
@@ -4,13 +4,13 @@ from .views import (vineyard_detail, vineyard_region, rr_form)
 
 app_name = 'vineyards'
 urlpatterns = [
-    path('<str:parent>/<str:region>/<slug:slug>/',
-         vineyard_detail, name="detail"),
-    path('<str:region>/<slug:slug>/',
-         vineyard_detail, name="detail-without-parent"),
     path('<str:parent>/<str:region>/<slug:slug>/form/',
          rr_form, name="detail-form"),
     path('<str:region>/<slug:slug>/form/',
          rr_form, name="detail-without-parent-form"),
+    path('<str:parent>/<str:region>/<slug:slug>/',
+         vineyard_detail, name="detail"),
+    path('<str:region>/<slug:slug>/',
+         vineyard_detail, name="detail-without-parent"),
     path('<slug:slug>/', vineyard_region, name="region"),
 ]
