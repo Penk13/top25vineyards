df8c98713fcf82c002b8a471269944f438ef4961
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-11-08 09:50:36 +0700

    improve profile form on profile page

--- accounts/views.py
+++ accounts/views.py
@@ -4,18 +4,17 @@ from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required
 from pages_app.models import ContentPage
 
+
 @login_required
 def profile(request):
     user = request.user
     profile = Profile.objects.get(user=user)
     form = ProfileForm(instance=profile)
     if request.method == 'POST':
-        print("POST")
-        form = ProfileForm(request.POST, instance=profile)
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
-            print("VALID")
             form.save()
-            return redirect("accounts:login")
+            return redirect("accounts:profile")
     content_page = get_object_or_404(ContentPage, types="SEARCH_PAGE")
     context = {
         'user': user,
