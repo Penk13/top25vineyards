b46b6a989781ccb6392e6609ca75e53be1e5d3a3
Penk13 <72376705+Penk13@users.noreply.github.com>
2021-11-12 23:04:20 +0700

    improve signals on vineyards rr form

--- vineyards/signals.py
+++ vineyards/signals.py
@@ -3,32 +3,47 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
 from .models import ReviewAndRating
 import datetime
 
 
 @receiver(post_save, sender=ReviewAndRating)
 def submission_email(sender, instance, **kwargs):
-    body = render_to_string(
-        'submission-email.html',
-        {
-            'username': instance.user.username,
-            'title': instance.title,
-            'date_created': datetime.datetime.now(),
-            'email': settings.DEFAULT_FROM_EMAIL
-        })
+    if instance.approved is True:
+        domain = Site.objects.get_current().domain
+        path = instance.vineyard.get_absolute_url()
+        subject = "Your review and rating are now live"
+        body = render_to_string(
+            'approval-email.html',
+            {
+                'username': instance.user.username,
+                'vineyard_url': domain + path,
+                'email': settings.DEFAULT_FROM_EMAIL
+            })
+    else:
+        subject = "Thank you for your rating and review"
+        body = render_to_string(
+            'submission-email.html',
+            {
+                'username': instance.user.username,
+                'title': instance.title,
+                'date_created': datetime.datetime.now(),
+                'email': settings.DEFAULT_FROM_EMAIL
+            })
     # Email sent to user
-    send_mail("Thank you for your rating and review",
+    send_mail(subject,
               body,
               settings.DEFAULT_FROM_EMAIL,
               [instance.user.email])
 
 
 @receiver(post_save, sender=ReviewAndRating)
-def approval_email(sender, instance, **kwargs):
-    # Email sent to admin
-    send_mail("Review from User",
-              instance.title + '\n\n' +
-              instance.review,
-              instance.user.email,
-              [settings.DEFAULT_FROM_EMAIL])
+def from_user_email(sender, instance, **kwargs):
+    if instance.approved is False:
+        # Email sent to admin
+        send_mail("Review from User",
+                  instance.title + '\n\n' +
+                  instance.review,
+                  instance.user.email,
+                  [settings.DEFAULT_FROM_EMAIL])
