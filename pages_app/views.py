800ad0696d3529d82746cfb0ac1667f6156d09f5
Penk13 <natanaelsteven109@gmail.com>
2022-02-16 09:49:22 +0700

    little fix on searchpage

--- pages_app/views.py
+++ pages_app/views.py
@@ -125,7 +125,7 @@ def footerpage(request, slug):
 def searchpage(request):
     content_page = get_object_or_404(ContentPage, types="SEARCH_PAGE")
     category = Category.objects.get(slug="global-travel-news")
-    travel_news = Post.objects.filter(category=category)
+    travel_news = Post.objects.filter(category=category).order_by("-id")
     billboards = Billboard.objects.filter(display=True)
 
     p = Paginator(Vineyard.objects.filter(
